c8ea229f4466b636f3094280b38f9c3d7ef6bf3e
Kevin Murray <booqweefius123@gmail.com>
2021-02-03 15:25:27 -0700

    added all events to the first page

--- script.js
+++ script.js
@@ -1,20 +1,54 @@
 let hiddenFontSize = 12;
+let count = 0
 
-function clickHandler(event){
-    switch(event){
-        case "nixon":
-            hiddenText.innerHTML = "Your vote has been cast!"
-        break;
+function onPageLoad(){
+    choose.innerHTML = "Choose Your Canidate!";
+}
 
-        case "mcGovern":
-            hiddenText.innerHTML = "an error has occured please try again"
-            hiddenFontSize += 10;
-            hiddenText.style.fontsize = hiddenFontSize;
-        break
+function clickNixon(){
+    hiddenText.innerHTML = "Your vote has been cast!";
+}
+
+function clickMcGovern(){
+    
+    if(count == 0){
+        choose.innerHTML = "I said choose your canidate..."
+        count++;
+    }
+    else if(count == 1){
+        McGovern.style.top = "10px";
+        choose.innerHTML = "up high!"
+        count++;
+    }
+    else if(count == 2){
+        McGovern.style.top = "250px";
+        choose.innerHTML = "down low!"
+        count++;
+    }
+    else if(count == 3){
+        McGovern.style.left = "600px"
+        choose.innerHTML = "too slow..."
+        count++;
+        console.log(count)
+    }
+    else if(count == 4){
+        McGovern.style.left = "705px"
+        McGovern.style.top = "34000px"
+        for(let i = 0; i < 1000; i++){
+            let text = document.createElement("p");
+            text.innerHTML = "CRIMINAL";
+            text.style.color = "red";
+            main.appendChild(text);
+        }
+        html.style.background = "url(images/maroPNGPNGPNG.jpg)";
+        html.style.backgroundSize = "100%"
     }
 }
 
+setTimeout(onPageLoad, 100);
+
+Nixon.addEventListener("click", clickNixon)
+McGovern.addEventListener("click", clickMcGovern)
 
 
-Nixon.addEventListener("click", clickHandler("nixon"))
-McGovern.addEventListener("click", clickHandler("mcGovern"))
+
